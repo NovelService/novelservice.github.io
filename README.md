16ba7fd7334e9672d7438755433b04c5dfd4cd85
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-08 20:31:15 +0200

    done

---
+++ README.md
@@ -0,0 +1,6 @@
+# NovelService
+Novel Service allows downloading webpages and turning then into ebooks.
+
+Visit https://novelservice.github.io
+
+This site servers as a minimal frontend to access the backend functionality.
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
-
 const postUrl = 'https://my6pmey7adsjxo46zbqxk3nw7e0nvazt.lambda-url.eu-central-1.on.aws/'
 const getUrl = 'https://35647flwmbq46rhlty6ulnbnhq0jyxni.lambda-url.eu-central-1.on.aws/'
 
+const postForm = document.getElementById('postForm')
 const postButton = document.getElementById('postSubmit')
 const postButtonLoading = document.getElementById('postSubmitLoading')
 const urlsTextArea = document.getElementById('urls')
 const idInput = document.getElementById('id')
+const getForm = document.getElementById('getForm')
 const getButton = document.getElementById('getSubmit')
 const getButtonLoading = document.getElementById('getSubmitLoading')
 const downloadA = document.getElementById('download')
@@ -17,92 +18,122 @@ function createUUID() {
     })
 }
 
+function addPostFormListener() {
+    postForm.addEventListener('submit', async (event) => {
+        event.preventDefault()
+        postButton.classList.add('d-none')
+        postButtonLoading.classList.remove('d-none')
 
-postButton.addEventListener('click', async (event) => {
-    event.preventDefault()
-    postButton.classList.add('d-none')
-    postButtonLoading.classList.remove('d-none')
-
-    const urls = urlsTextArea.value.trim().split('\n')
-    const id = createUUID()
-    const data = {
-        id: id,
-        urls: urls,
-        options: {
-            output: id + '.epub'
-        }
-    }
-    try {
-        await fetch(postUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-
-        idInput.value = id
-        getButton.click()
-    } finally {
-        postButton.classList.remove('d-none')
-        postButtonLoading.classList.add('d-none')
-    }
-})
-
-getButton.addEventListener("click", async (event) => {
-    event.preventDefault()
-    getButton.classList.add('d-none')
-    getButtonLoading.classList.remove('d-none')
-
-    async function pollUntilDone(id) {
-        const data = { 'id': id }
-        const response = await fetch(getUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-        console.log(response)
-        const jsonResponse = await response.json()
-        if (jsonResponse.status === 'done') {
-            done(jsonResponse.url)
-        } else {
-            setTimeout(pollUntilDone, 5000, id)
-        }
-    }
-
-    function done(url) {
-        downloadA.href = url
-        downloadA.click()
-
-        getButton.classList.remove('d-none')
-        getButtonLoading.classList.add('d-none')
-    }
-
-    const id = idInput.value
-
-    await pollUntilDone(id)
-})
-
-window.addEventListener('DOMContentLoaded', () => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav')
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        })
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler')
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    )
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click()
+        const urls = urlsTextArea.value.trim().split('\n')
+        const id = createUUID()
+        const data = {
+            id: id,
+            urls: urls,
+            options: {
+                output: id + '.epub'
             }
-        })
-    })
+        }
+        try {
+            await fetch(postUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            })
 
-});
+            idInput.value = id
+            getButton.click()
+        } finally {
+            postButton.classList.remove('d-none')
+            postButtonLoading.classList.add('d-none')
+        }
+    })
+}
+
+function addGetFormListener() {
+    getForm.addEventListener('submit', async (event) => {
+        event.preventDefault()
+        getButton.classList.add('d-none')
+        getButtonLoading.classList.remove('d-none')
+
+        async function pollUntilDone(id) {
+            const data = { 'id': id }
+            const response = await fetch(getUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            })
+            console.log(response)
+            const jsonResponse = await response.json()
+            if (jsonResponse.status === 'done') {
+                done(jsonResponse.url)
+            } else {
+                setTimeout(pollUntilDone, 5000, id)
+            }
+        }
+
+        function done(url) {
+            downloadA.href = url
+            downloadA.click()
+
+            getButton.classList.remove('d-none')
+            getButtonLoading.classList.add('d-none')
+        }
+
+        const id = idInput.value
+
+        await pollUntilDone(id)
+    })
+}
+
+function setupNavbar() {
+    window.addEventListener('DOMContentLoaded', () => {
+
+        // Activate Bootstrap scrollspy on the main nav element
+        const mainNav = document.body.querySelector('#mainNav')
+        if (mainNav) {
+            new bootstrap.ScrollSpy(document.body, {
+                target: '#mainNav',
+                offset: 74,
+            })
+        };
+
+        // Collapse responsive navbar when toggler is visible
+        const navbarToggler = document.body.querySelector('.navbar-toggler')
+        const responsiveNavItems = [].slice.call(
+            document.querySelectorAll('#navbarResponsive .nav-link')
+        )
+        responsiveNavItems.map(function (responsiveNavItem) {
+            responsiveNavItem.addEventListener('click', () => {
+                if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                    navbarToggler.click()
+                }
+            })
+        })
+
+    })
+}
+
+function addFormValidationListeners() {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach((form) => {
+            form.addEventListener('submit', (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+}
+
+setupNavbar()
+addFormValidationListeners()
+addPostFormListener()
+addGetFormListener()
